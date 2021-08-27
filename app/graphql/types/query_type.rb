ebf0e03db0068195881663695bec74e20fba4176
Patryk Ptasinski <patryk@ipepe.pl>
2021-08-27 15:55:17 +0200

    Add structurization with ItemsQueryType

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,28 +1,8 @@
 module Types
   class QueryType < Types::BaseObject
     field :me, Types::UserType, null: true
-    field :item,
-          Types::ItemType,
-          null: false,
-          description: "Reruns single item" do
-      argument :id, ID, required: true
-    end
-    field :items,
-          [Types::ItemType],
-          null: false,
-          description: "Returns a list of items in the martian library" do
-      argument :by_title, String, required: false
-    end
 
-    def item(id:)
-      Item.find(id)
-    end
-
-    def items(by_title: nil)
-      items = Item.preload(:user)
-      items = items.where(title: by_title) if by_title.present?
-      items
-    end
+    include ItemsQueryType
 
     def me
       context[:current_user]
